c8fe33ad99128cc2b845ce4a49f5c50b10fa93a9
Sylvain Galtier <sylvaingaltier@gmail.com>
2018-12-18 10:18:00 +0100

    controller articles done and cloudinary carreirwave gem installed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,48 @@
 class ArticlesController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:index, :new]
+  skip_before_action :authenticate_user!, only: [:index, :new]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user_id = current_user.id
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :profession, :content, :photo, :photo_cache)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_082828) do
+ActiveRecord::Schema.define(version: 2018_12_18_091423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_12_18_082828) do
     t.bigint "users_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["users_id"], name: "index_articles_on_users_id"
   end
 
